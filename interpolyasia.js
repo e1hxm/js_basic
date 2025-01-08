686599fb53750ea3a742fe9dc297eeba1fef91bb
El'ham <elham@ro.ru>
2025-01-09 02:59:15 +0300

    Пройдена лекция про интерполяцию в JavaScript

--- interpolyasia.js
+++ interpolyasia.js
@@ -1,2 +1,17 @@
-const  categories = 'toys';
+//const  categories = 'toys';
+//console.log(categories);
 
+//const name = "El'ham";
+//const age = 35;
+
+//const message = `Привет, меня зовут ${name}, и мне ${age} лет.`;
+//console.log(message);
+//// Привет, меня зовут El'ham, и мне 35 лет
+
+//const  categories = 'toys';
+//console.log(`https://someurl.com/${categories}`);
+//https://someurl.com/toys
+
+//const admin = "El'ham";
+//console.log(`Привет, ${admin}`);
+// Привет, El'ham
